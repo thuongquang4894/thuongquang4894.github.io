581cbbad9bc3ddbbcc0cb1f84403bf6e859e049a
tiennguyen <>
2025-04-25 14:31:52 +0700

    update

---
+++ src/settings.8b9f0.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/GameBull/scenes/Main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"1b082",main:"b7cb7"}};
